59dbf1a7bfd4fcf1c1ac3ca442dfb6822673ded5
arnelpocsedio <arnelpocsedio@gmail.com>
2020-11-08 14:33:44 +0800

    Added comments so the r codes are easier to understand

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
-#get data
+#This code creates plot1.png which is similar to the first plot 
+#in the Coursera assignment (Week 1 of Exploratory Data Analysis)
+
+#1. Get data
 getdata = function(fileURL) {
         if(!file.exists("./data")){dir.create("./data")}
         
@@ -13,32 +16,40 @@ fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power
 
 getdata(fileURL)
 
-#load data
+#2. Load data
 list.files("./data")
 mydataoverview = read.table("./data/household_power_consumption.txt", nrows = 10, sep = ";", header = TRUE)
+head(mydataoverview) #look at a small part of the data
+
+#load the dates
 mydatadates = read.table("./data/household_power_consumption.txt", colClasses = c(NA, rep("NULL", 8)), 
                          sep = ";", header = TRUE)
 
-
-
+#subset the relevant dates only
 start = min(which(mydatadates=="1/2/2007"))
 end = max(which(mydatadates=="2/2/2007"))
 
+#range of the relevant rows.
+start
+end
 end - start
 
+#read the data including relevant rows only
+#adding extra rows both in start and end to make sure to capture the relevant dates
 mydata = read.table("./data/household_power_consumption.txt", skip = 66630, nrows = 2890, 
                     sep = ";", header = TRUE, stringsAsFactors = F)
 
+#adding column labels
 names(mydata) = names(mydataoverview)
 
 head(mydata, n=10)
 tail(mydata, n=10)
 
-
+#final subsetting of the relevant dates
 mydata = subset(mydata, Date %in%  c("1/2/2007", "2/2/2007"))
 unique(mydata$Date)
 
-#plotting
+#3. plotting
 hist(mydata$Global_active_power, main = "Global Active Power", xlab = "Global Active power (kilowatts)",
      col = "red")
 
--- plot3.R
+++ plot3.R
@@ -1,4 +1,7 @@
-#get data
+#This code creates plot3.png which is similar to the third plot 
+#in the Coursera assignment (Week 1 of Exploratory Data Analysis)
+
+#1. Get data
 getdata = function(fileURL) {
         if(!file.exists("./data")){dir.create("./data")}
         
@@ -13,36 +16,44 @@ fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power
 
 getdata(fileURL)
 
-#load data
+#2. Load data
 list.files("./data")
 mydataoverview = read.table("./data/household_power_consumption.txt", nrows = 10, sep = ";", header = TRUE)
+head(mydataoverview) #look at a small part of the data
+
+#load the dates
 mydatadates = read.table("./data/household_power_consumption.txt", colClasses = c(NA, rep("NULL", 8)), 
                          sep = ";", header = TRUE)
 
-
-
+#subset the relevant dates only
 start = min(which(mydatadates=="1/2/2007"))
 end = max(which(mydatadates=="2/2/2007"))
 
+#range of the relevant rows.
+start
+end
 end - start
 
+#read the data including relevant rows only
+#adding extra rows both in start and end to make sure to capture the relevant dates
 mydata = read.table("./data/household_power_consumption.txt", skip = 66630, nrows = 2890, 
                     sep = ";", header = TRUE, stringsAsFactors = F)
 
+#adding column labels
 names(mydata) = names(mydataoverview)
 
 head(mydata, n=10)
 tail(mydata, n=10)
 
-
+#final subsetting of the relevant dates
 mydata = subset(mydata, Date %in%  c("1/2/2007", "2/2/2007"))
 unique(mydata$Date)
 
-#date and time column
+#3. formatting date and time column
 library(lubridate)
 mydata$Date_Time = dmy(mydata$Date)+hms(mydata$Time)
 
-#plotting
+#4. plotting
 plot(mydata$Date_Time, mydata$Sub_metering_1, type = "l", 
      ylab = "Energy sub metering", xlab="")
 
